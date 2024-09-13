f1accc662b39134e2027ed268f63e8836907d663
陈奇 <chenqi2257@foryou56.com>
2024-09-13 19:53:45 +0800

    robot commit

---
+++ src/main/java/com/entity/DingdanxinxiEntity.java
@@ -0,0 +1,328 @@
+package com.entity;
+
+import com.baomidou.mybatisplus.annotations.TableId;
+import com.baomidou.mybatisplus.annotations.TableName;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.lang.reflect.InvocationTargetException;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.apache.commons.beanutils.BeanUtils;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.enums.FieldFill;
+import com.baomidou.mybatisplus.enums.IdType;
+
+
+/**
+ * 订单信息
+ * 数据库通用操作实体类（普通增删改查）
+ * @author 
+ * @email 
+ * @date 2021-05-10 10:26:09
+ */
+@TableName("dingdanxinxi")
+public class DingdanxinxiEntity<T> implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+
+	public DingdanxinxiEntity() {
+		
+	}
+	
+	public DingdanxinxiEntity(T t) {
+		try {
+			BeanUtils.copyProperties(this, t);
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 主键id
+	 */
+	@TableId
+	private Long id;
+	/**
+	 * 订单编号
+	 */
+					
+	private String dingdanbianhao;
+	
+	/**
+	 * 房屋名称
+	 */
+					
+	private String fangwumingcheng;
+	
+	/**
+	 * 房屋类型
+	 */
+					
+	private String fangwuleixing;
+	
+	/**
+	 * 月租金
+	 */
+					
+	private String yuezujin;
+	
+	/**
+	 * 租赁时长
+	 */
+					
+	private Integer zulinshizhang;
+	
+	/**
+	 * 总价格
+	 */
+					
+	private Integer zongjiage;
+	
+	/**
+	 * 备注
+	 */
+					
+	private String beizhu;
+	
+	/**
+	 * 租赁日期
+	 */
+				
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd")
+	@DateTimeFormat 		
+	private Date zulinriqi;
+	
+	/**
+	 * 用户名
+	 */
+					
+	private String yonghuming;
+	
+	/**
+	 * 姓名
+	 */
+					
+	private String xingming;
+	
+	/**
+	 * 手机
+	 */
+					
+	private String shouji;
+	
+	/**
+	 * 是否审核
+	 */
+					
+	private String sfsh;
+	
+	/**
+	 * 审核回复
+	 */
+					
+	private String shhf;
+	
+	/**
+	 * 是否支付
+	 */
+					
+	private String ispay;
+	
+	
+	@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
+	@DateTimeFormat
+	private Date addtime;
+
+	public Date getAddtime() {
+		return addtime;
+	}
+	public void setAddtime(Date addtime) {
+		this.addtime = addtime;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * 设置：订单编号
+	 */
+	public void setDingdanbianhao(String dingdanbianhao) {
+		this.dingdanbianhao = dingdanbianhao;
+	}
+	/**
+	 * 获取：订单编号
+	 */
+	public String getDingdanbianhao() {
+		return dingdanbianhao;
+	}
+	/**
+	 * 设置：房屋名称
+	 */
+	public void setFangwumingcheng(String fangwumingcheng) {
+		this.fangwumingcheng = fangwumingcheng;
+	}
+	/**
+	 * 获取：房屋名称
+	 */
+	public String getFangwumingcheng() {
+		return fangwumingcheng;
+	}
+	/**
+	 * 设置：房屋类型
+	 */
+	public void setFangwuleixing(String fangwuleixing) {
+		this.fangwuleixing = fangwuleixing;
+	}
+	/**
+	 * 获取：房屋类型
+	 */
+	public String getFangwuleixing() {
+		return fangwuleixing;
+	}
+	/**
+	 * 设置：月租金
+	 */
+	public void setYuezujin(String yuezujin) {
+		this.yuezujin = yuezujin;
+	}
+	/**
+	 * 获取：月租金
+	 */
+	public String getYuezujin() {
+		return yuezujin;
+	}
+	/**
+	 * 设置：租赁时长
+	 */
+	public void setZulinshizhang(Integer zulinshizhang) {
+		this.zulinshizhang = zulinshizhang;
+	}
+	/**
+	 * 获取：租赁时长
+	 */
+	public Integer getZulinshizhang() {
+		return zulinshizhang;
+	}
+	/**
+	 * 设置：总价格
+	 */
+	public void setZongjiage(Integer zongjiage) {
+		this.zongjiage = zongjiage;
+	}
+	/**
+	 * 获取：总价格
+	 */
+	public Integer getZongjiage() {
+		return zongjiage;
+	}
+	/**
+	 * 设置：备注
+	 */
+	public void setBeizhu(String beizhu) {
+		this.beizhu = beizhu;
+	}
+	/**
+	 * 获取：备注
+	 */
+	public String getBeizhu() {
+		return beizhu;
+	}
+	/**
+	 * 设置：租赁日期
+	 */
+	public void setZulinriqi(Date zulinriqi) {
+		this.zulinriqi = zulinriqi;
+	}
+	/**
+	 * 获取：租赁日期
+	 */
+	public Date getZulinriqi() {
+		return zulinriqi;
+	}
+	/**
+	 * 设置：用户名
+	 */
+	public void setYonghuming(String yonghuming) {
+		this.yonghuming = yonghuming;
+	}
+	/**
+	 * 获取：用户名
+	 */
+	public String getYonghuming() {
+		return yonghuming;
+	}
+	/**
+	 * 设置：姓名
+	 */
+	public void setXingming(String xingming) {
+		this.xingming = xingming;
+	}
+	/**
+	 * 获取：姓名
+	 */
+	public String getXingming() {
+		return xingming;
+	}
+	/**
+	 * 设置：手机
+	 */
+	public void setShouji(String shouji) {
+		this.shouji = shouji;
+	}
+	/**
+	 * 获取：手机
+	 */
+	public String getShouji() {
+		return shouji;
+	}
+	/**
+	 * 设置：是否审核
+	 */
+	public void setSfsh(String sfsh) {
+		this.sfsh = sfsh;
+	}
+	/**
+	 * 获取：是否审核
+	 */
+	public String getSfsh() {
+		return sfsh;
+	}
+	/**
+	 * 设置：审核回复
+	 */
+	public void setShhf(String shhf) {
+		this.shhf = shhf;
+	}
+	/**
+	 * 获取：审核回复
+	 */
+	public String getShhf() {
+		return shhf;
+	}
+	/**
+	 * 设置：是否支付
+	 */
+	public void setIspay(String ispay) {
+		this.ispay = ispay;
+	}
+	/**
+	 * 获取：是否支付
+	 */
+	public String getIspay() {
+		return ispay;
+	}
+
+}
